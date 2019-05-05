27d038a04f89b782f5cd39b2071a6c04c493d4b0
Ricardo Bailoni <pr000rbailoni@prservicos.com.br>
2019-05-05 17:21:30 -0300

    Initial Commit

---
+++ GitStars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitStars
+//
+//  Created by Ricardo Bailoni on 05/05/19.
+//  Copyright © 2019 Ricardo Bailoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
